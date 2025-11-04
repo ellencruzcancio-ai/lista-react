3a5db0c21b85c55199b97a72e1344f8834058f92
ell3n <ellen.cruz.cancio@gmail.com>
2025-11-04 10:38:56 -0300

    Exercício 1 C

---
+++ 1) C.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function dataHoraAtual() {
+  const agora = new Date();
+  const diaSemana = ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'];
+  const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+  const diasSemana = diaSemana[agora.getDay()];
+  const dia = agora.getDate();
+  const mes = meses[agora.getMonth()];
+  const ano = agora.getFullYear();
+
+  return (
+    <h1>Hoje é {diasSemana}, {dia} de {mes} de {ano}</h1>
+  );
+}
+
+export default dataHoraAtual; 
